2b5054215d972147d5874f670ae77d801c7c6bd1
lalwende07 <nico0702legrand@gmail.com>
2022-08-06 13:54:10 +0200

    localNumber

--- src/pages/Posts.tsx
+++ src/pages/Posts.tsx
@@ -11,28 +11,34 @@ const Posts: React.FC = () => {
     const [allPosts, setAllPosts] = useState<Publish[] | null>(null);
     const [NumberOfPosts, setNumberOfPosts] = useState<number>(5);
     
+    const localOrStateNumber = () => localStorage.getItem('number') || NumberOfPosts;
+    const localNumber = localOrStateNumber();
+
 
     useEffect(() => {
         const getPosts = async () => {
-            const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${NumberOfPosts}`);
+            const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${localNumber}`);
             const datas: Publish[] = await response.json();
             //console.log(datas);
             setAllPosts(datas);
-            console.log(allPosts);
+            //console.log(allPosts);
         };
         getPosts();
-    }, [NumberOfPosts]);
+    }, [localNumber]);
 
     const onChange = (e:React.ChangeEvent<HTMLInputElement>) => {
         setNumberOfPosts(+e.target.value)
+
+        //save in localStorage number selected
+        localStorage.setItem('number', e.target.value)
     }
 
     return (
         <div className="posts-container">
             <h1>Page Principale</h1>
             <div style={{ display: "flex", flexDirection: 'column' }}>
-                <label htmlFor="posts"> Nombre de publication {NumberOfPosts}</label>
-                <input type="range" min={1} max={20} onChange={onChange} />
+                <label htmlFor="posts"> Nombre de publication {localNumber}</label>
+                <input type="range" min={1} max={20} onChange={onChange} defaultValue={localNumber} />
                 <PostsList allPosts={allPosts}/>
             </div>
         </div>
